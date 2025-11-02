69077223d1e3710361aa17612ffa00a84851c276
Kaarthikeyan Ganesh <kaarthikeyanganesh@gmail.com>
2025-11-02 08:46:38 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-9022291012-12182",
+  "appId": "1:492329213227:web:03b21c7c04bae4bf87f96e",
+  "apiKey": "AIzaSyDK962_IPdjS7GEDAxKBenVr5UjHBr_248",
+  "authDomain": "studio-9022291012-12182.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "492329213227"
+};
