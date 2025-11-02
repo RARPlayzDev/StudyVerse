a493a58ae18b416a36f285a2d89f9aeb0d995985
Kaarthikeyan Ganesh <kaarthikeyanganesh@gmail.com>
2025-11-02 10:32:05 +0000

    Try fixing this error: `Runtime Error: Error: useMemo is not defined. Er

--- src/app/(student)/dashboard/focus/page.tsx
+++ src/app/(student)/dashboard/focus/page.tsx
@@ -1,5 +1,5 @@
 'use client';
-import { useState, useEffect } from "react";
+import { useState, useEffect, useMemo } from "react";
 import PageTitle from "@/components/common/page-title";
 import { Button } from "@/components/ui/button";
 import { Card, CardContent, CardDescription, CardHeader, CardTitle } from "@/components/ui/card";
